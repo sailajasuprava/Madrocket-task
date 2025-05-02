3f1ee4f2a85a249f6a0fd9fea9841fd6b022802f
sailajasuprava <sailaja.suprava@gmail.com>
2025-05-02 10:45:31 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import PokemonDetails from "./pages/PokemonDetails";
 import Favorites from "./pages/Favorites";
+import AppLayout from "./components/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pokemons/:pokemonName" element={<PokemonDetails />} />
-        <Route path="/favorites" element={<Favorites />} />
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/pokemons/:pokemonName" element={<PokemonDetails />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
